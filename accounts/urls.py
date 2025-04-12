ef0aa7878bf31075d4dbf0a8ef9b8b2bc1ff77ce
Gustavo Guevra <gustavo.andres.guevara@gmail.com>
2025-04-13 00:46:11 +0200

    change logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .views import LoginView, UserProfileView, RegisterView
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('auth/login/', LoginView.as_view(), name='login'),
-    path('auth/logout/', LoginView.as_view(), name='logout'),
+    path('auth/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('auth/profile/', UserProfileView.as_view(), name='profile'),
     # Use Python comments, not JavaScript style
 ] 
