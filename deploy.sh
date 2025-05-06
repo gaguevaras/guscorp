53a156e050069b4808010eacbc652f2f45ad3edd
Gustavo Guevra <gustavo.andres.guevara@gmail.com>
2025-05-06 21:59:43 +0200

    update docker compose prod

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "This will restart the web_prod service with a fresh build."
+echo "This will restart all production services with a fresh build."
 read -p "Are you sure you want to proceed? (y/N): " confirm
 
 confirm=${confirm,,}
 
 if [[ "$confirm" == "y" || "$confirm" == "yes" ]]; then
     echo "Starting deployment..."
-    docker compose -f docker-compose.prod.yml up web_prod -d --force-recreate --build --remove-orphans
+    docker compose -f docker-compose.prod.yml up -d --force-recreate --build --remove-orphans
     echo "Deployment completed."
 else
     echo "Operation cancelled."
